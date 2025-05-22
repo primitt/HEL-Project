6e379b5b5daa6741cd1e12c570f2bce5f1af0ee6
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2025-05-22 00:42:09 -0700

    still more to do but....

---
+++ app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import Flask, request, render_template, send_from_directory
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return flask.send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
